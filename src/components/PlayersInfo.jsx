a0542fe0f27e599933561fcf667687f672128099
SaraCS21 <saracs15.scssn@gmail.com>
2023-02-08 12:43:45 +0000

    Página de los jugadores

---
+++ src/components/PlayersInfo.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function PlayersInfo() {
+    return (
+        <section className='w-full p-2 flex'>
+            <img className='w-2/5 h-20 bg-black' src="" alt=""/>
+            <div className='ml-5 mt-1'>
+                <p className='text-lg font-bold'>Lionel Messi</p>
+                <p className='font-bold text-[#8a8a8a]'>Forward</p>
+            </div>
+        </section>
+    )
+}
+
+export default PlayersInfo
