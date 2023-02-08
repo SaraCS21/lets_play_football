38c4eb5baf7bb714326cfd174d4e7504a77290e3
SaraCS21 <saracs15.scssn@gmail.com>
2023-02-08 12:44:19 +0000

    Página de los jugadores

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './styles/index.css'
 import Header from './components/Header'
 import TeamInfo from './components/TeamInfo'
+import PlayersInfo from './components/PlayersInfo'
 // import TeamContent from './components/TeamContent'
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       </header>
       <main>
         <TeamInfo/>
+        <PlayersInfo/>
       </main>
     </div>
   )
