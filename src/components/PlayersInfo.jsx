abcdc34b8c520a1b05c8c31f22a9753275609aef
Sara <saracs15.scss@gmail.com>
2023-02-10 15:42:27 +0000

    Arreglando errores

--- src/components/PlayersInfo.jsx
+++ src/components/PlayersInfo.jsx
@@ -1,22 +1,24 @@
 import React from 'react'
 
-function PlayersInfo({team}) {
-    const teamInfo = team.team;
-    const playersInfo = teamInfo.players;
+function PlayersInfo({ team }) {
+    const playersInfo = team.team?.players;
+
+    // const playersInfo = teamInfo.players;
+    console.log(playersInfo)
 
     return (
-        <div>
-            {/* {playersInfo.map((player => {
+        playersInfo ? <div className='w-full flex flex-wrap'>
+            {playersInfo.map((player => 
                 <section className='w-full xl:w-1/4 p-2 xl:m-8 flex'>
-                    <img className='w-2/5 h-20 xl:h-24 bg-black' src={player.photo} alt={player.name}/>
+                    <img className='w-2/5 h-20 xl:h-24 bg-black' src={player.photo} alt={player.name} />
                     <div className='ml-5 xl:ml-7 mt-1'>
                         <p className='text-lg font-bold'>{player.surname} {player.name}</p>
-                        <p className='font-bold text-[#8a8a8a]'>{position.name}</p>
+                        <p className='font-bold text-[#8a8a8a]'>{player.position}</p>
                     </div>
                 </section>
-                }))
-            } */}
-        </div>
+            ))
+            }
+        </div> : null
     )
 }
 
